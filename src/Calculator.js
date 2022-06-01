5d57530e6716ea2aaee1d973757ec4b1d44daf5b
Talha <tkz9696@gmail.com>
2022-06-01 18:41:39 +0500

    Remove Boilerplate; Skeleton Code Added

---
+++ src/Calculator.js
@@ -0,0 +1,12 @@
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div id="challenge-node"><h1>My First React Component!</h1></div>
+        );
+    }
+}
+
+export default Calculator;
