8d6162153288a5dfd399b3ebaefc7a79a8f65bc2
Talha <tkz9696@gmail.com>
2022-06-02 02:19:56 +0500

    Add UI and Code

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React from 'react';
 import Calculator from './Calculator';
 
 class App extends React.Component {
--- src/Calculator.js
+++ src/Calculator.js
@@ -1,12 +1,37 @@
+/* eslint-disable react/prefer-stateless-function */
+import './Calculator.css';
+import React from 'react';
+
 class Calculator extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return(
-            <div id="challenge-node"><h1>My First React Component!</h1></div>
-        );
-    }
+  render() {
+    return (
+      <div className="calculator">
+        <input type="text" className="calculator-screen" value="0" disabled />
+
+        <div className="calculator-keys">
+          <button type="button" className="btn" value="all-clear">     AC</button>
+          <button type="button" className="btn" value="+/-">          +/-</button>
+          <button type="button" className="btn" value="%">              %</button>
+          <button type="button" className="btn orange" value="/">&divide;</button>
+          <button type="button" className="btn" value="7">              7</button>
+          <button type="button" className="btn" value="8">              8</button>
+          <button type="button" className="btn" value="9">              9</button>
+          <button type="button" className="btn orange" value="*">       X</button>
+          <button type="button" className="btn" value="4">              4</button>
+          <button type="button" className="btn" value="5">              5</button>
+          <button type="button" className="btn" value="6">              6</button>
+          <button type="button" className="btn orange" value="-">       -</button>
+          <button type="button" className="btn" value="1">              1</button>
+          <button type="button" className="btn" value="2">              2</button>
+          <button type="button" className="btn" value="3">              3</button>
+          <button type="button" className="btn orange" value="+">       +</button>
+          <button type="button" className="btn double" value="0">       0</button>
+          <button type="button" className="btn" value=".">              .</button>
+          <button type="button" className="btn orange" value="=">       =</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Calculator;
